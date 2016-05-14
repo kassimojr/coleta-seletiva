e352261321bc3a15dc47a949463bb111f073292b
kassimojr <kassimojr@outlook.com>
2016-05-14 15:56:31 -0300

    Classes de Persistência para Usuario, Coleta e Cooperativa

---
+++ coletaSeletiva/src/main/java/br/com/unibratec/coletaseletiva/persistencia/CooperativaDAO.java
@@ -0,0 +1,9 @@
+package br.com.unibratec.coletaseletiva.persistencia;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.unibratec.coletaseletiva.entidades.Cooperativa;
+
+public interface CooperativaDAO extends CrudRepository<Cooperativa, Long>{
+
+}
